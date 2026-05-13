414186e3508f3475d46ba3eae32b6fab185f212b
Aliesy1628 <gulaku1628@gmail.com>
2026-05-13 09:38:35 +0700

    Constructor produk

--- Produk.java
+++ Produk.java
@@ -2,5 +2,10 @@ public abstract class Produk {
     private String nama;
     private double harga;
     private int stok;
-    
+
+    public Produk(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
 }
