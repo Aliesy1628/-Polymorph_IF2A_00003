138535ba17c28798c8f661c107441ce499d563e6
Aliesy1628 <gulaku1628@gmail.com>
2026-05-13 10:01:02 +0700

    Kasir

---
+++ Kasir.java
@@ -0,0 +1,25 @@
+public class Kasir {
+
+    private String namaKasir;
+
+    public Kasir(String namaKasir) {
+        this.namaKasir = namaKasir;
+    }
+
+    public void tampilIdentitas() {
+        System.out.println("====== TOKO GARUDA ======");
+        System.out.println("Nama Kasir: " + namaKasir);
+    }
+
+    public void prosesBelanja(Produk p, int jmlhBeli) {
+        if(p.getStok() < jmlhBeli) {
+            System.out.println("Out of Stok");
+        } {
+            double hargaAkhir = p.hitungHargaAkhir(jmlhBeli);
+            System.out.println("Harga Akhir = " + hargaAkhir);
+            p.setStok(jmlhBeli);
+        }
+
+        System.out.println("Stok terbaru = " + p.getStok());
+    } 
+}
