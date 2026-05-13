9c1b9aba19f4fdebdbefdb1900e7a3a9a1fad016
Aliesy1628 <gulaku1628@gmail.com>
2026-05-13 09:52:20 +0700

    hitunghargaakhir pada Makanan

--- Makanan.java
+++ Makanan.java
@@ -1,3 +1,9 @@
 public class Makanan extends Produk {
+
+    @Override
+    public double hitungHargaAkhir(int jmlhBeli) {
+        System.out.println("Total harga=" + hitungTotalHarga(jmlhBeli));
+        return hitungTotalHarga(jmlhBeli) * 0.95;
+    }
     
 }
